568f959c08e69b9829eaeb4e9e3ff3948ee66091
kefrankefran7609 <teamecologia@gmail.com>
2024-06-19 15:18:24 -0600

    Create layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import '@/assets/styles/globals.css'
+
+const MainLayout = () => {
+  return (
+    <html lang="en">
+        <body>
+    <div>MainLayout</div>
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout
